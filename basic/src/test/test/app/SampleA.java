e07de88738fff8a9054e9df442679dd69221f017
speedDev <speedDev@DESKTOP-MA5O6IS>
2021-12-12 14:32:35 +0900

    test

---
+++ basic/src/test/test/app/SampleA.java
@@ -0,0 +1,14 @@
+package test.app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SampleA {
+
+	@Test
+	void testA() {
+		System.out.println(">>> sampleA +++");
+	}
+
+}
